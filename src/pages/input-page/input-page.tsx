ebabee483eec9a5a93f49297f29aa1c6a51e2df1
AlexanderSergan <veteroque@gmail.com>
2018-08-30 03:48:44 +0300

    Input Api reference, usage an demo

--- src/pages/input-page/input-page.tsx
+++ src/pages/input-page/input-page.tsx
@@ -1,37 +1,134 @@
-import { Component, Listen, State } from '@stencil/core';
-
+import { Component, Listen, State } from "@stencil/core";
 
 @Component({
-    tag: 'input-page',
-    styleUrl: 'input-page.scss'
+  tag: "input-page",
+  styleUrl: "input-page.scss"
 })
 export class InputPage {
+  @State()
+  inputResult: string = undefined;
 
-    @State() inputResult: string = undefined;
+  @Listen("onvaluechange")
+  customEventHandler(event: CustomEvent): void {
+    console.log("Received the custom event: ", event);
+    this.inputResult = event.detail;
+  }
 
-    @Listen('onvaluechange')
-    customEventHandler(event: CustomEvent): void {
-        console.log('Received the custom event: ', event);
-        this.inputResult = event.detail
-    }
+  render() {
+    return (
+      <div class="container">
+        <h1>Input component</h1>
 
-    render() {
-        return (
-            <div class="container">
+        <h2 class="mt-3">Overview</h2>
 
-                <h2>Input component demo</h2>
+        <p>
+          <code>CwcInput</code> is a component to render simple bootstrap input
+          in your template.
+        </p>
 
-                <cwc-input label="Demo label" value={'My shiny value'}>
+        <h2 class="mt-3">Props</h2>
 
-                </cwc-input>
+        <div class="row">
+          <div class="col-lg-12">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Param</th>
+                  <th>Type</th>
+                  <th>Default</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>value</td>
+                  <td>
+                    <code>String</code>
+                  </td>
+                  <td>''</td>
+                  <td>Initial value of input</td>
+                </tr>
+                <tr>
+                  <td>label</td>
+                  <td>
+                    <code>String</code>
+                  </td>
+                  <td>''</td>
+                  <td>Labal of the input</td>
+                </tr>
+                <tr>
+                  <td>
+                    <code>onvaluechange</code>
+                  </td>
+                  <td>
+                    <code>CustomEvent</code>
+                  </td>
+                  <td>''</td>
+                  <td>
+                    Value emited every input change and available in{" "}
+                    <code>`event.detail`</code> field.
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
 
-                {
-                    this.inputResult &&
-                    <span class="mt-2">Catched event with data: {this.inputResult}</span>
-                }
+        <h2 class="mt-3">Usage</h2>
 
-            </div >
+        <p>Stencil usage: </p>
 
-        );
-    }
+        <pre>{this.usageStencil}</pre>
+
+        <p>
+          Angular usage: <a href="http://stackblitz.com">Stackblitz demo</a>.
+        </p>
+
+        <p>Vanilla html usage: </p>
+
+        <pre>{this.usageHtml}</pre>
+
+        <h2 class="mt-3">Input component demo</h2>
+
+        <cwc-input label="Demo label" value={"My shiny value"} />
+
+        {this.inputResult && (
+          <span class="mt-2">Catched event with data: {this.inputResult}</span>
+        )}
+      </div>
+    );
+  }
+
+  usageStencil = `
+        <cwc-input label="Demo label" value={"My shiny value"} />
+        `;
+  usageHtml = `
+        <body>
+            <cwc-input>
+
+            </cwc-input>
+            <ul class="output-target">
+                // Results available here.
+            </ul>
+
+            <script>
+                const cmp = document.querySelector('cwc-input')
+                cmp.value = 'Shiny value'
+                cmp.label = 'Input label'
+
+
+                var outputTarget = document.querySelector('ul.output-target')
+
+                cmp.addEventListener('onvaluechange', e => {
+                    const newChild = document.createElement('LI')
+
+                    newChild.textContent = 'Catched an event with data "' + event.detail + '"'
+                    outputTarget.appendChild(newChild)
+                })
+
+
+            </script> 
+        </body>
+
+        `;
 }
